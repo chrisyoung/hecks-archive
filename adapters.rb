beb884296842b08e502e3d25c0904f2cbafd5e17
Chris Young <beesucker@gmail.com>
2016-08-08 01:00:24 -0700

    remove dups

--- adapters.rb
+++
@@ -1,2 +0,0 @@
-require_relative 'adapters/drivers/web_api'
-require_relative 'adapters/clients/sql_database'
--- domain.rb
+++
@@ -1,3 +0,0 @@
-require_relative "domain/value_object"
-require_relative "domain/use_case"
-require_relative "domain/aggregate"
