39afbaeeab9bed51944638af3691c00a349f047a
Chris Young <beesucker@gmail.com>
2016-09-14 07:52:13 -0700

    add folder for generating aggregate

---
+++ lib/generators/aggregate.rb
@@ -0,0 +1,25 @@
+class Hecks < Thor
+  class Aggregate < Thor::Group
+    include Thor::Actions
+
+    argument :name
+
+    class_option :head_name, :aliases => "-h", :desc => "the name of the aggregate head"
+    class_option :attributes, :aliases => "-a", type: :hash, :desc => "attributes for the aggregate head"
+    def self.source_root
+      File.dirname(__FILE__)
+    end
+
+    def head_name
+      options[:head_name]
+    end
+
+    def attributes
+      options[:attributes]
+    end
+
+    def create_aggregate_folder
+      directory('templates/aggregate', '.')
+    end
+  end
+end
