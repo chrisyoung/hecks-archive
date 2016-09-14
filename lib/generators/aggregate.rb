d1a9f0fd246fbe48970e1085829eef2e39c2c1d3
Chris Young <beesucker@gmail.com>
2016-09-14 08:17:05 -0700

    generators for aggregate and head

--- lib/generators/aggregate.rb
+++ lib/generators/aggregate.rb
@@ -6,6 +6,7 @@ class Hecks < Thor
 
     class_option :head_name, :aliases => "-h", :desc => "the name of the aggregate head"
     class_option :attributes, :aliases => "-a", type: :hash, :desc => "attributes for the aggregate head"
+
     def self.source_root
       File.dirname(__FILE__)
     end
@@ -14,8 +15,28 @@ class Hecks < Thor
       options[:head_name]
     end
 
+    def head_module_name
+      head_name.camelize
+    end
+
+    def attribute_names_as_strings
+      attributes.keys.map {|key| key.to_s}
+    end
+
+    def keys_and_values
+      attributes.map {|key, value| (key.to_s + ': ' + key.to_s)}.join(", ")
+    end
+
+    def attribute_names
+      attributes.keys.map {|key| ':' + key.to_s}.join ", "
+    end
+
+    def attribute_param_names
+      attributes.keys.map {|key| key.to_s + ':'}.join ", "
+    end
+
     def attributes
-      options[:attributes]
+      options[:attributes].merge({id: 'integer'})
     end
 
     def create_aggregate_folder
