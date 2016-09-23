429f9f334a633235b4b168d6cd4f53391164d49a
Chris Young <beesucker@gmail.com>
2016-09-22 18:09:06 -0700

    update hecks binary and add schema stuff in to spec

--- spec/pizza_server_demo_hexagon/lib/schemas/pizzas/create.rb
+++ spec/pizza_server_demo_hexagon/lib/schemas/pizzas/create.rb
@@ -2,9 +2,9 @@ module PizzaServerDemoHexagon
   module Schemas
     module Pizzas
       Create = Dry::Validation.Schema do
-        # required(:name).filled(:str?)
-        # required(:description).filled(:str?)
-        # required(:toppings).each(:str?)
+        required(:name).filled(:str?)
+        required(:description).filled(:str?)
+        required(:toppings).each(:str?)
       end
     end
   end
