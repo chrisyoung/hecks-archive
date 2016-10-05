ab2c423fe9a0ff590277674b4ef4e35339dd5766
Chris Young <beesucker@gmail.com>
2016-10-04 20:27:19 -0700

    clean up app spec

--- spec/lib/ports/app_spec.rb
+++ spec/lib/ports/app_spec.rb
@@ -7,30 +7,48 @@ describe Hecks::Ports::Left::App do
   let(:domain)        { PizzaServer }
   let(:fake_listener) { FakeListener }
   let(:pizza_attributes) do
-    { name: "White Pizza", description: 'white sauce and chicken', toppings: [{ name: 'chicken' }] }
+    {
+      name: "White Pizza",
+      description: 'white sauce and chicken',
+      toppings: [{ name: 'chicken' }]
+    }
   end
 
   subject { described_class.new(listeners: [fake_listener], domain: domain) }
 
   describe '#call' do
     it 'calls the method on the repository' do
-      result = subject.call(command_name: :create, module_name: :pizzas, args: pizza_attributes)
+      subject.call(
+        command_name: :create,
+        module_name:  :pizzas,
+        args:         pizza_attributes
+      )
+
       expect(
         subject.query(
           query_name: :find_by_id,
           module_name: :pizzas,
-          args: {id: 1}).name
-        ).to eq 'White Pizza'
+          args: {id: 1}
+        ).name
+      ).to eq 'White Pizza'
     end
 
     it 'broadcasts the command over the events port' do
       expect(FakeListener).to receive(:pizzas_create)
-      subject.call(command_name: :create, module_name: :pizzas, args: { name: "White Pizza", description: 'white sauce and chicken', toppings: [{ name: 'chicken' }] })
+      subject.call(
+        command_name: :create,
+        module_name: :pizzas,
+        args: pizza_attributes)
     end
 
     it 'runs validations' do
-      result = subject.call(command_name: :create, module_name: :pizzas, args: { name: 1 } )
-      expect(result.errors.first).to include("did not contain a required property of 'description'")
+      message = "did not contain a required property of 'name'"
+      result = subject.call(
+        command_name: :create,
+        module_name: :pizzas,
+        args: { }
+      )
+      expect(result.errors.first).to include(message)
     end
   end
 end
