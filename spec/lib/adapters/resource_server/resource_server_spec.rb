65d29223ea58d66bd8797f028037b70196ea609b
Chris Young <beesucker@gmail.com>
2016-10-23 11:46:40 -0700

    make tests more declarative

--- spec/lib/adapters/resource_server/resource_server_spec.rb
+++ spec/lib/adapters/resource_server/resource_server_spec.rb
@@ -1,41 +1,39 @@
 describe Hecks::Adapters::ResourceServer::App do
-  let(:id) {JSON.parse(create_pizza.body)["id"]}
+  let(:id) { JSON.parse(last_response.body)['id'] }
 
-  it 'reads aggregates' do
-    create_pizza
-    expect(JSON.parse(read_pizza(id).body)['name']).to eq 'The Yuck'
+  it 'reads a resource' do
+    post '/pizzas', attributes.to_json
+    get "/pizzas/#{id}"
+    name = JSON.parse(last_response.body)['name']
+    expect(name).to eq 'The Yuck'
   end
 
-  it 'creates aggregates' do
-    expect(JSON.parse(create_pizza.body)["id"]).to be
+  it 'creates a resource' do
+    post '/pizzas', attributes.to_json
+    expect(JSON.parse(last_response.body)["id"]).to be
   end
 
-  it 'updates aggregates' do
-    put "/pizzas/#{id}", attributes('The Double Yuck').to_json
-    expect(JSON.parse(read_pizza(id).body)["name"]).to eq "The Double Yuck"
+  it 'updates a resource' do
+    post '/pizzas', attributes.to_json
+    put "/pizzas/#{id}", attributes.merge(name: "The Double Yuck").to_json
+    get "/pizzas/#{id}"
+    expect(JSON.parse(last_response.body)["name"]).to eq "The Double Yuck"
   end
 
-  it 'deletes aggregates' do
+  it 'deletes a resource' do
+    post '/pizzas', attributes.to_json
     delete "/pizzas/#{id}"
-    expect(JSON.parse(read_pizza(id).body)).to be_nil
+    get "/pizzas/#{id}"
+    expect(JSON.parse(last_response.body)).to be_nil
   end
 
   private
 
-  def read_pizza(id)
-    get "/pizzas/#{id}"
-  end
-
-  def create_pizza(name="The Yuck")
-    attributes.merge(name: name)
-    post '/pizzas', attributes.to_json
-  end
-
-  def attributes(name="The Yuck")
+  def attributes
     {
-      name: name,
+      name: 'The Yuck',
       description: "Worse than it sounds",
-      toppings: [{ name: 'children'}, {name: 'crickets'}]
+      toppings: [{ name: 'children'}, { name: 'crickets' }]
     }
   end
 end
