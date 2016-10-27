96638c8a593f3b60c37757ba9f93e35f1479eb83
Chris Young <beesucker@gmail.com>
2016-10-27 07:34:19 -0700

    wip

--- spec/pizza_builder/lib/domain/pizzas/schema.rb
+++ spec/pizza_builder/lib/domain/pizzas/schema.rb
@@ -1,1 +1,16 @@
-Dir[File.dirname(__FILE__) + '/schema/*.rb'].each { |file| require file }
+# Dir[File.dirname(__FILE__) + '/schema/*.rb'].each { |file| require file }
+
+# require_relative 'schema/pizza'
+
+module PizzaBuilder
+  module Domain
+    module Pizzas
+      module Schema
+        Head =
+          {
+            '$schema': 'http://json-schema.org/draft-04/schema',
+          }.merge(Schema::Pizza)
+      end
+    end
+  end
+end
