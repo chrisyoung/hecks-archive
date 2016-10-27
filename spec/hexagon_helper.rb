e178f8e2a0722639ee7a89b04bd490c770637fa0
Chris Young <beesucker@gmail.com>
2016-10-27 08:01:02 -0700

    remove cruft

--- spec/hexagon_helper.rb
+++ spec/hexagon_helper.rb
@@ -5,12 +5,6 @@ require 'rack/test'
 require 'pry'
 
 require_relative '../lib/hecks/'
-if ENV['GENERATE']
-  puts 'Generating Pizza Builder...'
-  `cd spec && rm -rf pizza_builder`
-  `cd spec && ../bin/hecks new -n pizza_builder -s pizza_builder_schema.json`
-  puts 'Done!'
-end
 
 require_relative 'pizza_builder/lib/pizza_builder'
 
