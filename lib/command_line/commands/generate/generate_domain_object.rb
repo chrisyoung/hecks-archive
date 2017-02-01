11cf7cda6bfd91fb77d64047bdf97a993bc1b355
Chris Young <beesucker@gmail.com>
2017-01-31 18:49:51 -0700

    extract formatting into class

--- lib/command_line/commands/generate/generate_domain_object.rb
+++ lib/command_line/commands/generate/generate_domain_object.rb
@@ -3,6 +3,7 @@
 class GenerateDomainObject < Thor::Group; end
 
 require_relative "generate_domain_object/assignment_template"
+require_relative "generate_domain_object/option_formatter"
 
 class GenerateDomainObject
   include Thor::Actions
@@ -24,118 +25,40 @@ class GenerateDomainObject
 
   private
 
+  def assignment_template(attributes)
+    AssignmentTemplate.new(attributes).render
+  end
+
+  def option_format(format, include_id: false)
+    OptionFormatter.new(options[:attributes]).call(format, include_id: include_id)
+  end
+
+  def head_name
+    options[:head_name]
+  end
+
   def name
     options[:name]
   end
 
-  def referenced_entity
-    options[:referenced_entity]
-  end
-
-  def underscored_name
-    options[:name].underscore
-  end
-
-  def camelized_name
-    name.camelize
-  end
-
-  # Aliases
-  def aggregate_name
-    module_name
-  end
-
-  def aggregate_module_name
-    camelized_module_name
-  end
-
-  def head_module_name
-    camelized_head_name
-  end
-
-  def attribute_names
-    attributes.map(&:name)
+  def file_name
+    name.underscore
   end
 
   def attribute_names_without_id
     attributes_without_id.map(&:name)
   end
 
-
-  def attribute_param_names
-    attribute_param_names_as_string
-  end
-
-  def attribute_param_names_without_id
-    attribute_param_names_as_string_without_id
-  end
-
-  # Head Name
-  def head_name
-    options[:head_name]
-  end
-
-  def camelized_head_name
-    head_name.camelize
-  end
-
-  # Module Name
   def module_name
     options[:module_name]
   end
 
-  def camelized_module_name
-    module_name.camelize
-  end
-
-  def keys_and_values_without_id
-    attributes_without_id.map { |attribute| (attribute.name + ': ' + attribute.name) }.join(', ')
-  end
-
-  def keys_and_values
-    attributes.map { |attribute| (attribute.name + ': ' + attribute.name) }.join(', ')
-  end
-
-  # Domain
   def domain_name
     Dir.pwd.split('/').last
   end
 
-  def assignment_template(attributes)
-    AssignmentTemplate.new(attributes).render
-  end
-
-  def parse_type(type)
-    type.delete('[').delete(']')
-  end
-
-  def camelized_domain_name
-    domain_name.camelize
-  end
-
-  # Attributes
-  def attribute_names_as_strings
-    attributes.map(&:name).join(', ')
-  end
-
-  def attributes_as_string
-    attributes.map { |attribute| ':' + attribute.name }.join ', '
-  end
-
   def attributes_without_id_as_string
     attributes_without_id.map { |attribute| ':' + attribute.name }.join ', '
-  end
-
-  def required_attributes_as_string
-    attributes_without_id.map { |attribute| ':' + attribute.name }.join ', '
-  end
-
-  def attribute_param_names_as_string
-    attributes.map { |attribute| attribute.name + ':' }.join ', '
-  end
-
-  def attribute_param_names_as_string_without_id
-    attributes_without_id.map { |attribute| attribute.name + ':' }.join ', '
   end
 
   def attributes
---
+++ lib/command_line/commands/generate/generate_domain_object/option_formatter.rb
@@ -0,0 +1,30 @@
+class GenerateDomainObject
+  class OptionFormatter
+    def initialize(attributes)
+      @attributes = attributes.map do |attribute|
+        Hecks::DomainBuilder::Attribute.new(attribute)
+      end
+    end
+
+    def call(format, include_id: false)
+      case format
+      when 'keys_and_values'
+        attributes(include_id).map { |attribute| (attribute.name + ': ' + attribute.name) }.join(', ')
+      when 'attribute_string'
+        attributes(include_id).map { |attribute| ':' + attribute.name }.join ', '
+      when 'param_names'
+        attributes(include_id).map { |attribute| attribute.name + ':' }.join ', '
+      end
+    end
+
+    private
+
+    def attributes(include_id)
+      if include_id
+        @attributes + [Hecks::DomainBuilder::Attribute.new(['id', 'value'])]
+      else
+        @attributes
+      end
+    end
+  end
+end
