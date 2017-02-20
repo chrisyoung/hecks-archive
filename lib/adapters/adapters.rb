3b9a83b36270c4ec5dfac2c94886769cf3090c11
Chris Young <beesucker@gmail.com>
2017-02-19 22:22:51 -0700

    move application out of adapters

--- lib/adapters/adapters.rb
+++ lib/adapters/adapters.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 require_relative 'memory_database/memory_database'
-require_relative 'application/application'
 require_relative 'resource_server/resource_server'
 require_relative 'events/events'
 require_relative 'logger/logger'
