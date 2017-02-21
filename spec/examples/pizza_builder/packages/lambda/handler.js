9c6f6062f5fc0380381440368ac4a34f7d25aafc
Chris Young <beesucker@gmail.com>
2017-02-21 07:19:50 -0700

    successfull calling the binary

--- spec/examples/pizza_builder/packages/lambda/handler.js
+++ spec/examples/pizza_builder/packages/lambda/handler.js
@@ -1,23 +1,14 @@
 'use strict';
 
-var invokeRubyApp = "./pizza_builder/pizza_builder";
 const spawn = require('child_process').spawn;
 
 module.exports.hello = (event, context, callback) => {
-  var child = spawn(invokeRubyApp, [JSON.stringify(event, null, 2), JSON.stringify(context, null, 2)]);
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: 'Go Serverless v1.0! Your function executed successfully!',
-  //     input: event,
-  //   }),
-  // };
+  var command = './pizza_builder/pizza_builder pizzas ' + "'" + JSON.stringify(event) + "'"
+  const child = exec(command, (result) => {
+        // Resolve with result of process
+        context.done(result);
+    });
 
-  child.stdout.on('data', function (data) { console.log("stdout:\n"+data); });
-  child.stderr.on('data', function (data) { console.log("stderr:\n"+data); });
-
-  // callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  // child.stdout.on('data', function (data) { console.log("stdout:\n"+data); });
+  // child.stderr.on('data', function (data) { console.log("stderr:\n"+data); });
 };
