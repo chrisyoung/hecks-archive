3f0070e5261b11158d6c804caac6b0e7092849cc
Chris Young <beesucker@gmail.com>
2017-04-01 16:17:30 -0700

    move vcr stuff into fixtures folder

--- spec/integration_helper.rb
+++ spec/integration_helper.rb
@@ -1,18 +1,11 @@
 require 'vcr'
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = "spec/integration/http_interactions"
   config.hook_into :webmock
 end
 
 RSpec.configure do |config|
-  config.before(:all) { |example|
-     VCR.insert_cassette("cache")
-
-
-  }
-
-  config.after(:all) { |example|
-      VCR.eject_cassette("cache")
-  }
+  config.before(:all) { |example| VCR.insert_cassette("cache") }
+  config.after(:all) { |example| VCR.eject_cassette("cache") }
 end
