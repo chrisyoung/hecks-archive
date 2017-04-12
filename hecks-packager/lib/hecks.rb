d0804cc9a20c1ff108d17a780bd02047d2cfabcf
Chris Young <beesucker@gmail.com>
2017-04-11 20:14:08 -0700

    fix problem with readline, fix parameter handling

--- hecks-packager/lib/hecks.rb
+++ hecks-packager/lib/hecks.rb
@@ -1,9 +1,32 @@
 require 'bundler'
+require 'getoptlong'
+
 Bundler.require
 
 require_relative 'args'
 require_relative 'compatibility/fixnum'
 require_relative 'app_runner'
 
-HecksApp = load('Hecksfile')
-puts AppRunner.new(args: ARGV, application: HecksApp).call.result
+load('Hecksfile')
+
+opts = GetoptLong.new(
+  [ '--module', '-m', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--command', '-c', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--data', '-d', GetoptLong::REQUIRED_ARGUMENT ]
+)
+
+domain_module = ''
+command = ''
+data = ''
+opts.each do |opt, arg|
+  case opt
+  when '--module'
+    domain_module = arg
+  when '--command'
+    command = arg
+  when '--data'
+    data = arg
+  end
+end
+
+puts AppRunner.new(args: [domain_module, command, data], application: HecksApp).call.result.to_json
