73632d60eabe0ebc4b5e68035f1ac799225c3a11
Chris Young <beesucker@gmail.com>
2017-04-11 20:09:24 -0700

    only create command for now

--- hecks-examples/pizza_builder/serverless/pizzas.js
+++ hecks-examples/pizza_builder/serverless/pizzas.js
@@ -15,30 +15,3 @@ module.exports.create = (event, context, callback) => {
     callback(null, response);
   });
 };
-
-module.exports.read = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Read', input: event }),
-  };
-
-  callback(null, response);
-};
-
-module.exports.update = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Update', input: event }),
-  };
-
-  callback(null, response);
-};
-
-module.exports.delete = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Delete', input: event}),
-  };
-
-  callback(null, response);
-};
