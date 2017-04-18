72b67d86d6ac40763b64484a75827aa152c79dd7
Chris Young <beesucker@gmail.com>
2017-04-17 19:37:41 -0700

    rename folder

--- bin/ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xe
-bash bin/install
-bash bin/ci/generate_pizza_builder
-bash bin/ci/package
-bash bin/ci/serverless_generation
-bash bin/ci/serverless_production
-rspec spec/unit
-USE_HECKS_GEMS=true rspec spec/integration
