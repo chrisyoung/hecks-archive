00fa252639b415bea751f14f71c7ec9148bf5987
Chris Young <beesucker@gmail.com>
2017-05-10 20:32:34 -0700

    get rid of some cruft

--- spec/hecks_helper.rb
+++ spec/hecks_helper.rb
@@ -4,14 +4,15 @@ ENV['HECKS_DOMAIN_PATH'] ||= File.dirname(__FILE__) + '/../example/pizza_builder
 
 # Adding these folders to the load path allows specs to be run
 # with no gems installed, ie. require hecks-application
-[ '/../adapters/dynamodb/lib/',
-  '/../adapters/resource_server/lib/',
-  '/../adapters/memory_database/lib/',
-  '/../adapters/sql_database/lib/',
-  '/../application/lib/',
-  '/../domain/lib/' ].each do |file|
-  $LOAD_PATH.unshift(File.dirname(__FILE__) + file)
-end
+%w{ /../adapters/dynamodb/lib/
+    /../adapters/resource_server/lib/
+    /../adapters/memory_database/lib/
+    /../adapters/sql_database/lib/
+    /../application/lib/
+    /../domain/lib/
+  }.each do |file|
+    $LOAD_PATH.unshift(File.dirname(__FILE__) + file)
+  end
 
 require_relative '../lib/hecks'
 require_relative '../example/pizza_builder/lib/pizza_builder'
