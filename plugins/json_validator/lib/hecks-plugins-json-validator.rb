fae716d7e0f23f8eee63ef1c82e83aee0ecb78c3
Chris Young <beesucker@gmail.com>
2017-11-15 09:04:16 -0700

    require the schema library and set up initialization

--- plugins/json_validator/lib/hecks-plugins-json-validator.rb
+++ plugins/json_validator/lib/hecks-plugins-json-validator.rb
@@ -1,4 +1,23 @@
+require 'json-schema'
+
 module HecksPlugins
   class JSONValidator
+    attr_reader :errors
+
+    def initialize(command:)
+      @args = command.args
+      @head_spec = command.domain_module.head
+      @errors = {}
+      validate
+    end
+
+    def call
+      self
+    end
+
+    private
+
+    def validate
+    end
   end
 end
