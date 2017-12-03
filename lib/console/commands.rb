e6d9141b7b8daf6f43536ae3477a401f6e6ae5b5
Chris Young <beesucker@gmail.com>
2017-12-03 12:01:56 -0700

    fix up hecks_console

--- lib/console/commands.rb
+++ lib/console/commands.rb
@@ -2,7 +2,20 @@ module HecksConsole
   # mixin methods to the console
   module Commands
     def app
-      @app ||= HecksApplication.new(domain: HecksConsole.domain)
+      @app ||= HecksApp
+    end
+
+    def domain_modules
+      DOMAIN.domain_modules
+    end
+
+    def status
+      ap({
+        domain_name: DOMAIN.name,
+        domain_module_names: DOMAIN.domain_modules.map { |dm| dm[0] },
+        validator: app.validator,
+        database: app.database
+      })
     end
   end
 end
