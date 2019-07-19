1de78b39402b2e8e5e359a07e52f586bd14dc66b
Chris Young <beesucker@gmail.com>
2019-07-19 08:56:07 -0700

    include some files

--- example/pizza_builder/lib/next_domain/domain_objects.rb
+++ example/pizza_builder/lib/next_domain/domain_objects.rb
@@ -1,4 +1,6 @@
-
+require_relative 'domain_object_methods/entity'
+require_relative 'domain_object_methods/value'
+require_relative 'domain_object_methods/subtypes'
 require_relative 'aggregate/domain_object'
 
 class NextDomain
